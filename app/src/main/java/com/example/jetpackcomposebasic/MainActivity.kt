0852114dd07a0882dff422d49299b03eb708bff5
STORM-PC\Takum <takumi87jn@gmail.com>
2024-02-13 22:28:17 +0900

    onClick prosess

--- app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -41,87 +42,48 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Column {
-                inputText()
+                pressJudge()
             }
         }
     }
 }
-data class LongStory(val part:String,val textColor:Int,val textSize:Int)
 
 @Composable
-fun appScreen() {
-    val longStory = listOf(
-        LongStory("1年目 " ,R.color.cherry_rose,20),
-        LongStory("成長口実 ",R.color.weak_violet,20),
-        LongStory("重労働 ",R.color.moegi,20),
-             LongStory("鯱川畜木",R.color.dark_red,16)
-            )
-    LazyColumn {
-        items(longStory){story ->
-            Text(text = story.part, color = colorResource(id = story.textColor), fontSize = story.textSize.sp)
-        }
-    }
-}
-@Composable
-fun buttonBackGround(){
-    Text(text = stringResource(id = R.string.hello_world), fontSize = 16.sp, modifier = Modifier
-        .background(
-            color = colorResource(id = R.color.deep_sky_blue),
-            RoundedCornerShape(20.dp)
-        )
-        .border(2.dp, Color.Black, RoundedCornerShape(20.dp))
-        .padding(20.dp))
-}
-@Composable
-fun showImage(){
+fun pressJudge(){
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        var countTapStar by remember{
-            mutableStateOf(0)
+        var isButtonPressed by remember{ mutableStateOf(false)}
+        QuestionComposable{
+            isButtonPressed=true
         }
-        var countTapTriangle by remember{
-            mutableStateOf(0)
+        if (isButtonPressed){
+            ResponseComposable()
         }
-        Row{
-            Text(text = "Star:$countTapStar ,")
-            Text(text = "Triangle:$countTapTriangle")
-        }
-        Row{
-            Image(
-                painter = painterResource(id = R.drawable.icon_svg), contentDescription = "",
-                modifier = Modifier
-                    .size(108.dp)
-                    .border(1.dp, color = colorResource(id = R.color.moegi))
-                    .clickable { countTapStar++ },
-                contentScale = ContentScale.Crop,
-            )
-            Image(
-                painter = painterResource(id = R.drawable.triangle),
-                contentDescription ="",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(108.dp)
-                    .clickable { countTapTriangle++ })
-        }
-        Text(text = stringResource(id = R.string.let_me_show),
-            color = colorResource(id = R.color.white),
-            fontSize = 16.sp,
-            modifier = Modifier.background(color = colorResource(id = R.color.deep_sky_blue)))
     }
 }
 @Composable
-fun inputText(){
-    Box(modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.TopCenter){
-        var userText by remember{ mutableStateOf("")}
-        OutlinedTextField(value =userText, onValueChange = {userText = it},
-            modifier = Modifier.padding(20.dp),
-            placeholder = {Text(text="input any word")},
-            maxLines = 5
-        )
+fun QuestionComposable(onClick:()-> Unit = {}) {
+    Text(
+        text = stringResource(id = R.string.announce_click),
+        color = colorResource(id = R.color.apple_green),
+        fontSize = 20.sp,
+        modifier = Modifier.padding(12.dp)
+    )
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .padding(horizontal = 12.dp, vertical = 4.dp)
+    ) {
+        Text(text = stringResource(id = R.string.click_button))
     }
 }
-
+@Composable
+fun ResponseComposable(){
+    Text(
+        text = stringResource(id = R.string.clicked_button),
+        color = colorResource(id =R.color.chartreuse_green),
+        fontSize = 20.sp
+    )
+}
