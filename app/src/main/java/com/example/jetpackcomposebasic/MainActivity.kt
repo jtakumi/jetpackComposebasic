8e576e400d058c0eace78ac4fee86a6369476605
STORM-PC\Takum <takumi87jn@gmail.com>
2024-03-03 19:27:52 +0900

    文字列を見やすくするように修正

--- app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasic/MainActivity.kt
@@ -4,22 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -28,11 +19,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -41,29 +30,38 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Column {
-               showImage()
+                showImage()
             }
         }
     }
 }
+
 @Composable
-fun showImage(){
+fun showImage() {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        var countTapStar by remember{
+        var countTapStar by remember {
             mutableStateOf(0)
         }
-        var countTapTriangle by remember{
+        var countTapTriangle by remember {
             mutableStateOf(0)
         }
-        Row{
-            Text(text = "Star:$countTapStar ,")
-            Text(text = "Triangle:$countTapTriangle")
+        Row {
+            Text(
+                text = "Star:$countTapStar ,",
+                fontSize = 16.sp,
+                color = colorResource(id = R.color.cherry_rose)
+            )
+            Text(
+                text = "Triangle:$countTapTriangle",
+                fontSize = 16.sp,
+                color = colorResource(id = R.color.moegi)
+            )
         }
-        Row{
+        Row {
             Image(
                 painter = painterResource(id = R.drawable.icon_svg), contentDescription = "",
                 modifier = Modifier
@@ -74,16 +72,12 @@ fun showImage(){
             )
             Image(
                 painter = painterResource(id = R.drawable.triangle),
-                contentDescription ="",
+                contentDescription = "",
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .size(108.dp)
                     .clickable { countTapTriangle++ })
         }
-        Text(text = stringResource(id = R.string.let_me_show),
-            color = colorResource(id = R.color.white),
-            fontSize = 16.sp,
-            modifier = Modifier.background(color = colorResource(id = R.color.deep_sky_blue)))
     }
 }
 
