ea587e9784065eea3b59dc81f1000d6e150725af
STORM-PC\Takum <takumi87jn@gmail.com>
2024-04-14 20:34:55 +0900

    不要な色を削除

--- app/src/main/java/com/example/jetpackcomposebasic/ui/theme/Color.kt
+++ app/src/main/java/com/example/jetpackcomposebasic/ui/theme/Color.kt
@@ -2,25 +2,6 @@ package com.example.jetpackcomposebasic.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-
-val SecondaryColor = Color(0xFF03DAC6)
-val SecondaryContainerColor = Color(0xFF018786)
-
-val DarkModeBG = Color(0xFF121212)
-
-val ErrorColor = Color(0xFFB00020)
-val DarkErrorColor = Color(0xFFCF6679)
 
 // # -> 0xFF
 // primary color(base color)=#19753a
